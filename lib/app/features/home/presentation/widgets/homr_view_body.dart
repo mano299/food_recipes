01c830e6ecc46f992d45e69b67d9ef977ff3bdb9
mano299 <mano299ahmed1@gmail.com>
2025-08-08 06:05:13 +0300

    create home view body

---
+++ lib/app/features/home/presentation/widgets/homr_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomrViewBody extends StatelessWidget {
+  const HomrViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        
+      ],
+    );
+  }
+}
