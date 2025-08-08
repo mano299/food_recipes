2fef46c0db18a769103cfa2f6290c1cf292b6b85
abdallahelkady <abdallahelkady06@gmail.com>
2025-08-08 06:51:20 +0300

    make register page at form to validate

--- lib/app/features/welcome/presentation/widgets/register_page_body.dart
+++ lib/app/features/welcome/presentation/widgets/register_page_body.dart
@@ -11,85 +11,92 @@ class RegisterPageBody extends StatelessWidget {
     super.key,
   });
   String? name, email, phoneNumber, password;
+
+  GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SvgPicture.asset("assets/photos/foodLogo.svg"),
-            Customtextfield(
-              hint: "Full Name",
-              icon: Icons.person,
-              onChanged: (value) {
-                name = value;
-              },
-            ),
-            Customtextfield(
-              hint: "Valied Email",
-              icon: Icons.email,
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            Customtextfield(
-              hint: "Phone Number",
-              icon: Icons.phone_iphone_outlined,
-              onChanged: (value) {
-                phoneNumber = value;
-              },
-            ),
-            Customtextfield(
-              hint: "Strong Password",
-              icon: Icons.lock_outline,
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            CustomButton(
-              label: "Register",
-              onTap: () async {
-                try {
-                  await RegisterUser();
+      child: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SvgPicture.asset("assets/photos/foodLogo.svg"),
+              Customtextfield(
+                hint: "Full Name",
+                icon: Icons.person,
+                onChanged: (value) {
+                  name = value;
+                },
+              ),
+              Customtextfield(
+                hint: "Valied Email",
+                icon: Icons.email,
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              Customtextfield(
+                hint: "Phone Number",
+                icon: Icons.phone_iphone_outlined,
+                onChanged: (value) {
+                  phoneNumber = value;
+                },
+              ),
+              Customtextfield(
+                hint: "Strong Password",
+                icon: Icons.lock_outline,
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              CustomButton(
+                label: "Register",
+                onTap: () async {
+                  if (formKey.currentState!.validate()) {
+                    try {
+                      await RegisterUser();
 
-                  log("message");
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'weak-password') {
-                    print('The password provided is too weak.');
-                  } else if (e.code == 'email-already-in-use') {
-                    print('The account already exists for that email.');
+                      log("message");
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'weak-password') {
+                        print('The password provided is too weak.');
+                      } else if (e.code == 'email-already-in-use') {
+                        print('The account already exists for that email.');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-            SizedBox(
-              height: 18,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already a member ",
-                  style: TextStyle(color: Colors.white),
-                ),
-                InkWell(
-                    onTap: () async {},
-                    child: Text(
-                      "Login",
-                      style: TextStyle(color: Color(0xff0A2527)),
-                    ))
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-          ],
+                },
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already a member ",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  InkWell(
+                      onTap: () async {},
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Color(0xff0A2527)),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
