726a3fed74bc600ad7d768f2d89f716f1f5eb5bd
mano299 <mano299ahmed1@gmail.com>
2025-08-11 16:04:25 +0300

    edit bottom nav bar

--- lib/app/features/home/presentation/widgets/bottom_navigation_bar.dart
+++ lib/app/features/home/presentation/widgets/bottom_navigation_bar.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:food_recipes/app/core/utils/colors.dart';
 
-class CustomNavigationBar extends StatelessWidget {
+class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
   final Function(int) onTap;
 
-  const CustomNavigationBar({
+  const CustomBottomNavigationBar({
     super.key,
     required this.currentIndex,
     required this.onTap,
@@ -15,29 +14,36 @@ class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      iconSize: 26,
       showSelectedLabels: false,
       showUnselectedLabels: false,
-      unselectedItemColor: Colors.black,
-      backgroundColor: Colors.blue,
-      selectedIconTheme: const IconThemeData(color: kPrimaryColor),
+      unselectedIconTheme: IconThemeData(
+        color: Colors.black,
+        size: 22,
+      ),
+      selectedIconTheme: IconThemeData(
+        color: kPrimaryColor,
+        size: 28,
+      ),
+      backgroundColor: Colors.white,
       currentIndex: currentIndex,
       onTap: onTap,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
-          label: ''
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.magnifyingGlass),
-          label: ''
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.shuffle),
-          label: ''
+          label: '',
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.search),
-          label: ''
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.radar),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_pin),
+          label: '',
         ),
       ],
     );
--- lib/app/features/home/presentation/widgets/home_view_body.dart
+++ lib/app/features/home/presentation/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:food_recipes/app/core/utils/colors.dart';
 import 'package:food_recipes/app/core/utils/styles.dart';
 import 'package:food_recipes/app/features/home/presentation/home_view.dart';
 import 'package:food_recipes/app/features/home/presentation/search_view.dart';
+import 'package:food_recipes/app/features/home/presentation/widgets/bottom_navigation_bar.dart';
 import 'package:food_recipes/app/features/home/presentation/widgets/category_list_view.dart';
 import 'package:food_recipes/app/features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:food_recipes/app/features/home/presentation/widgets/featured_list_view.dart';
@@ -24,112 +25,84 @@ class _HomeViewBodyState extends State<HomeViewBody> {
     const HomeView(),
     const SearchView(),
     const RandomView(),
-    const HomeView(),
+    const Center(child: Text('Profile'),),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 26,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        unselectedIconTheme: IconThemeData(
-          color: Colors.black,
-          size: 22,
-        ),
-        selectedIconTheme: IconThemeData(
-          color: kPrimaryColor,
-          size: 28,
-        ),
-        backgroundColor: Colors.white,
+      bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (index) {
           setState(() {
-            currentIndex = index; 
+            currentIndex = index;
           });
         },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.radar),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_pin),
-            label: '',
-          ),
-        ],
       ),
       backgroundColor: Colors.white,
-      body: currentIndex == 0 ? SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CustomAppBar(),
-            const SizedBox(height: 24),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24.0),
-              child: Text(
-                'Featured',
-                style: Styles.textStyle21(context),
-              ),
-            ),
-            const SizedBox(height: 12),
-            const FeaturedListView(),
-            const SizedBox(height: 24),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: currentIndex == 0
+          ? SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Category',
-                    style: Styles.textStyle21(context),
-                  ),
-                  TextButton(
-                    onPressed: () {},
+                  const CustomAppBar(),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
                     child: Text(
-                      'See All',
-                      style: Styles.textStyle14(context).copyWith(
-                        color: kPrimaryColor,
+                      'Featured',
+                      style: Styles.textStyle21(context),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  const FeaturedListView(),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Category',
+                          style: Styles.textStyle21(context),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'See All',
+                            style: Styles.textStyle14(context).copyWith(
+                              color: kPrimaryColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  const CategoryListView(),
+                  const SizedBox(height: 24),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0),
+                    child: PopularRecipesHeader(),
+                  ),
+                  const SizedBox(height: 12),
+                  SizedBox(
+                    height: 270,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 24.0),
+                      child: ListView.builder(
+                        itemCount: 5,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return const PopularListViewItem();
+                        },
                       ),
                     ),
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 12),
-            CategoryListView(),
-            SizedBox(height: 24),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: PopularRecipesHeader(),
-            ),
-            SizedBox(height: 12),
-            SizedBox(
-              height: 270,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 24.0),
-                child: ListView.builder(
-                  itemCount: 5,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return PopularListViewItem();
-                  },
-                ),
-              ),
-            ),
-            
-          ],
-        ),
-      ) : pages[currentIndex],
+            )
+          : pages[currentIndex],
     );
   }
 }
