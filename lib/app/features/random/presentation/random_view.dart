e9d2705eb31736c5e4875204e84e166987b6f24d
mano299 <mano299ahmed1@gmail.com>
2025-08-11 14:55:15 +0300

    create random feature

---
+++ lib/app/features/random/presentation/random_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:food_recipes/app/features/random/presentation/widgets/random_view_body.dart';
+
+class RandomView extends StatelessWidget {
+  const RandomView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RandomViewBody();
+  }
+}
