2096087b702e2994575bd419bf31ba0cfb22c436
mano299 <mano299ahmed1@gmail.com>
2025-08-11 15:38:37 +0300

    create random meal card

---
+++ lib/app/features/random/presentation/widgets/random_meal_card.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import 'package:food_recipes/app/core/utils/styles.dart';
+
+class RandomMealCard extends StatelessWidget {
+  const RandomMealCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Card(
+        
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                child: Image(
+                  image: AssetImage('assets/photos/Image 1.png',),
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.7,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text('Sunny Egg & Toast Avocado', style: Styles.textStyle18(context),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/features/random/presentation/widgets/random_view_body.dart
+++ lib/app/features/random/presentation/widgets/random_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_recipes/app/core/utils/colors.dart';
 import 'package:food_recipes/app/core/utils/styles.dart';
+import 'package:food_recipes/app/features/random/presentation/widgets/random_meal_card.dart';
 
 class RandomViewBody extends StatelessWidget {
   const RandomViewBody({super.key});
@@ -14,6 +15,14 @@ class RandomViewBody extends StatelessWidget {
         title: Text(
           'Random Meal',
           style: Styles.textStyle28(context).copyWith(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RandomMealCard(),
+          ],
         ),
       ),
     );
