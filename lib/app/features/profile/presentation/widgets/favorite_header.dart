890310adb6a7536de35392feeebba60c5be9f7e4
mano299 <mano299ahmed1@gmail.com>
2025-08-12 10:35:53 +0300

    add favorite header

---
+++ lib/app/features/profile/presentation/widgets/favorite_header.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:food_recipes/app/core/utils/styles.dart';
+import 'package:food_recipes/app/features/home/presentation/views/widgets/see_all_button.dart';
+
+class FavoriteHeader extends StatelessWidget {
+  const FavoriteHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'My Favorites',
+          style:
+              Styles.textStyle21(context).copyWith(fontWeight: FontWeight.w900),
+        ),
+        SeeAllButton(
+          text: "See All",
+        ),
+      ],
+    );
+  }
+}
--- lib/app/features/profile/presentation/widgets/profile_view_body.dart
+++ lib/app/features/profile/presentation/widgets/profile_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:food_recipes/app/core/utils/styles.dart';
+import 'package:food_recipes/app/features/profile/presentation/widgets/favorite_header.dart';
 import 'package:food_recipes/app/features/profile/presentation/widgets/user_info.dart';
 
 class ProfileViewBody extends StatelessWidget {
@@ -12,14 +13,16 @@ class ProfileViewBody extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: profileAppBar(context),
-        body: Column(
-          children: [
-            SizedBox(height: 32),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: UserInfo(),
-            ),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            children: [
+              SizedBox(height: 26),
+              UserInfo(),
+              SizedBox(height: 24),
+              FavoriteHeader()
+            ],
+          ),
         ),
       ),
     );
