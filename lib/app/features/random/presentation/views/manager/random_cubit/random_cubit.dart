f07a7918e65b9d53f8aa6deca913e4e35d996129
mano299 <mano299ahmed1@gmail.com>
2025-08-18 00:45:21 +0300

    complete random cubit

--- lib/app/features/random/presentation/views/manager/random_cubit/random_cubit.dart
+++ lib/app/features/random/presentation/views/manager/random_cubit/random_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:food_recipes/app/features/home/data/models/meal_model.dart';
 
@@ -6,4 +7,26 @@ part 'random_state.dart';
 
 class RandomCubit extends Cubit<RandomState> {
   RandomCubit() : super(RandomInitial());
+
+  final Dio dio = Dio();
+
+  Future<void> fetchRandomMeal() async {
+    emit(RandomLoading());
+    try {
+      final response =
+          await dio.get("https://www.themealdb.com/api/json/v1/1/random.php");
+
+      if (response.statusCode == 200) {
+        final data = response.data;
+        final meals = data["meals"] as List;
+        final meal = MealModel.fromJson(meals.first);
+
+        emit(RandomSuccess(meal));
+      } else {
+        emit(RandomFailure("Failed with status: ${response.statusCode}"));
+      }
+    } catch (e) {
+      emit(RandomFailure(e.toString()));
+    }
+  }
 }
--- lib/app/features/random/presentation/views/manager/random_cubit/random_state.dart
+++ lib/app/features/random/presentation/views/manager/random_cubit/random_state.dart
@@ -1,21 +1,28 @@
 part of 'random_cubit.dart';
 
-sealed class RandomState extends Equatable {
+abstract class RandomState extends Equatable {
   const RandomState();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-final class RandomInitial extends RandomState {}
-final class RandomLoading extends RandomState {}
-final class RandomSuccess extends RandomState {
+class RandomInitial extends RandomState {}
+
+class RandomLoading extends RandomState {}
+
+class RandomSuccess extends RandomState {
   final MealModel meal;
   const RandomSuccess(this.meal);
-}
-final class RandomFailure extends RandomState {
-  final String errMessage;
 
+  @override
+  List<Object?> get props => [meal];
+}
+
+class RandomFailure extends RandomState {
+  final String errMessage;
   const RandomFailure(this.errMessage);
-  
+
+  @override
+  List<Object?> get props => [errMessage];
 }
