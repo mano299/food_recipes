c893d419d4f48685843642f1f5e39c4ad1ab3694
mano299 <mano299ahmed1@gmail.com>
2025-08-21 01:15:35 +0300

    delete unnessacary files

--- lib/app/features/home/presentation/views/widgets/search_meals_grid_view.dart
+++ lib/app/features/home/presentation/views/widgets/search_meals_grid_view.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_recipes/app/core/utils/circular_indicator.dart';
 import 'package:food_recipes/app/features/home/presentation/manager/search_cubit/search_cubit.dart';
 import 'package:food_recipes/app/features/home/presentation/views/widgets/search_meal_item.dart';
 
@@ -30,7 +31,7 @@ class SearchMealsGridView extends StatelessWidget {
           );
         } else if (state is SearchLoading) {
           return SliverToBoxAdapter(
-              child: Center(child: CircularProgressIndicator()));
+              child: Center(child: CircularIndicator()));
         } else {
           return SliverToBoxAdapter(
             child: SizedBox(),
